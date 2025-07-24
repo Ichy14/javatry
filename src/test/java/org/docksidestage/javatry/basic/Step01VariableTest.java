cf441498dbee5daa5ca3c1f11ef30d9f22179b86
jflute <dbflute@gmail.com>
2025-07-24 18:01:05 +0900

    step1のビッグバンドビートまでレビュー: 思考コメントありがとうございます！わからない細かいところは1on1にてフォローしますね。

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -27,7 +27,7 @@ import org.docksidestage.unit.PlainTestCase;
  * @author Ichy14 (n.ichikawa@bizreach.co.jp)
  */
 public class Step01VariableTest extends PlainTestCase {
-// `exrends`キーワードを使うによりStep01VariableTestはPlainTestCaseを継承している
+    // `exrends`キーワードを使うによりStep01VariableTestはPlainTestCaseを継承している
     // ===================================================================================
     //                                                                      Local Variable
     //                                                                      ==============
@@ -39,6 +39,7 @@ public class Step01VariableTest extends PlainTestCase {
         String sea = "mystic";
         log(sea); // your answer? => mystic
         // TODO: `log()`メソッドの実装を読解したい
+        // TODO ichikawa [ふぉろー] よーし、1on1で一緒に読みますか(^^ by jflute (2025/07/24)
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -53,6 +54,15 @@ public class Step01VariableTest extends PlainTestCase {
         // intの8がstringにキャストされた？stringで宣言したnullが"null"という文字列として変換されているのはなぜ?
         // `+`演算子を実行するときは左側の型に合わせて演算される＆String型が左右どちらかに来たらString型として処理する、みたいなことになってそう？
         // TODO: 変数の型を確認する方法は？
+        // TODO ichikawa [ふぉろー] 演算中の型の理解はそれでOKです。Stringが含まれたらそこにどんどん引きずりこまれる感じですね by jflute (2025/07/24)
+        // +演算子の型変換は暗黙で行われるので、IDE上でもあんまり確認する方法がないかもですね。
+        // まあ例えばこう書いてみて...
+        //  e.g. int a = 1 + "b";
+        // → コンパイルエラーのメッセージに「Type mismatch: cannot convert from String to int」とあるので、
+        // これで String になっていることを確認する、ってことはできそうです。
+
+        // TODO jflute 1on1にて、null文字列に変換されることに関してフォロー予定 (2025/07/24)
+        // (↑これはくぼ用のtodoということでそのまま残しておいてください)
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -86,6 +96,10 @@ public class Step01VariableTest extends PlainTestCase {
         log(sea); // your answer? => 417
         // BigDecimalはimmutable → seaの値は変わらない。Python書いてたときにもこんなのがあった気がする
         // Javaのsyntax sugarのこともうちょい知りたい
+        // TODO ichikawa [いいね] immutableという概念を理解されているの素晴らしいです by jflute (2025/07/24)
+        // TODO ichikawa [ふぉろー] 1on1のときぜひsyntax sugarの話させてください by jflute (2025/07/24)
+
+        // TODO jflute 1on1にてimmutable周りとJavaのsyntax sugarの話 (2025/07/24)
     }
 
     // ===================================================================================
@@ -103,6 +117,12 @@ public class Step01VariableTest extends PlainTestCase {
         // クラス内で宣言されたフィールドを取得しようとする関数？
         // instanceBroadwayという変数を宣言したものの、初期化していないので、型に合わせて自動的に初期値がセットされる
         // その初期値はString型ならnullになるので、nullが出力されるっぽい
+        // TODO ichikawa [ふぉろー] 初期値に関してはそれで認識大丈夫です by jflute (2025/07/24)
+        // クラス内で宣言されたフィールドは「インスタンス変数」と呼び、特定の一つのインスタンスに紐づく変数となります。
+        // そして、このtest_メソッドは文法的にはインスタンスメソッドと呼ばれて、
+        // 同じく特定の一つのインスタンスに紐づくメソッドということでインスタンス変数が参照できます。
+        // ということで、インスタンスメソッド内からインスタンス変数を直接参照しているということなので、
+        // 特に関数を経由して取得しているとかではありません。
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -116,18 +136,26 @@ public class Step01VariableTest extends PlainTestCase {
         Integer sea = instanceHangar;
         log(sea); // your answer? => nul
         // 基本型のintと違って、Integerはnullが許容されるっぽい？
+        // TODO ichikawa [ふぉろー] yes, そうなります。 by jflute (2025/07/24)
+        // intのようなプリミティブ型と、Integerのようなオブジェクト型と二つありまして、
+        // プリミティブ型は null という状態 (変数が何も指し示していない) が発生しないようになっています。
+        // ゆえに、intの場合はデフォルトで0が暗黙的に入るのですが、Integer/Stringなどのオブジェクト型は、
+        // (インスタンス変数としては) デフォルトで値が何も入らないということで null となります。
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_via_method() {
-        instanceBroadway = "bbb";  // BigBandBeat無くなるの悲しいです
+        instanceBroadway = "bbb"; // BigBandBeat無くなるの悲しいです
+        // TODO ichikawa [ざつだん] いやほんとその通り。It don't mean a thing がもう聴けない（＞＜ by jflute (2025/07/24)
         instanceMagiclamp = "magician";
         helpInstanceVariableViaMethod(instanceMagiclamp);
         String sea = instanceBroadway + "|" + instanceDockside + "|" + instanceHangar + "|" + instanceMagiclamp;
         log(sea); // your answer? => bbb|1|null|magician
         // ミュータブルな変数instanceDockside以外は、helpInstanceVariableViaMethodメソッドを呼び出して代入しても値は変わらないはず?
         // → 違った。なぜ？たぶん何かを勘違いしている。
-        //
+        // TODO ichikawa [ふぉろー] インスタンス変数はどのインスタンスメソッドからも参照ができるので... by jflute (2025/07/24)
+        // instanceBroadway = "bigband"; にて値が差し替わります。
+        // test_ も help も instanceBroadway は共有している同じ変数なので。
     }
 
     private void helpInstanceVariableViaMethod(String instanceMagiclamp) {
