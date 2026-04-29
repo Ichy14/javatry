ba3271ce842cb6ef8e2c5e853e4e36447c6be702
Ichy14 <n.ichikawa@bizreach.co.jp>
2026-04-29 23:37:11 +0900

    test_objectOriented_polymorphism_makeConcrete

--- src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
@@ -18,11 +18,7 @@ package org.docksidestage.javatry.basic;
 import org.docksidestage.bizfw.basic.buyticket.Ticket;
 import org.docksidestage.bizfw.basic.buyticket.TicketBooth;
 import org.docksidestage.bizfw.basic.buyticket.TicketType;
-import org.docksidestage.bizfw.basic.objanimal.Animal;
-import org.docksidestage.bizfw.basic.objanimal.BarkedSound;
-import org.docksidestage.bizfw.basic.objanimal.Cat;
-import org.docksidestage.bizfw.basic.objanimal.Dog;
-import org.docksidestage.bizfw.basic.objanimal.Zombie;
+import org.docksidestage.bizfw.basic.objanimal.*;
 import org.docksidestage.bizfw.basic.objanimal.loud.AlarmClock;
 import org.docksidestage.bizfw.basic.objanimal.loud.Loudable;
 import org.docksidestage.bizfw.basic.objanimal.runner.FastRunner;
@@ -383,6 +379,9 @@ public class Step06ObjectOrientedTest extends PlainTestCase {
      */
     public void test_objectOriented_polymorphism_makeConcrete() {
         // your confirmation code here
+        Seal seal = new Seal();
+        BarkedSound sound = seal.bark();
+        log(sound.getBarkWord());
     }
 
     /**
